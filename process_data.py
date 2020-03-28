8a2306477162b562edec299fb8b8793ce0c7c98b
thanhlt998 <thanhlt998@gmail.com>
2020-03-28 17:36:42 +0700

    Update process + news_classifier

--- process_data.py
+++ process_data.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 import string
+import numpy as np
+from sklearn.utils import shuffle
 
 
 def list2text(texts):
@@ -25,14 +27,17 @@ def process(dir_name, output_dir, cols: list, test_pct=0.2, sep='\t'):
         sub_df = df[cols]
         sub_df.fillna(value='', inplace=True)
         sub_df['text'] = sub_df.apply(lambda x: list2text(x), axis=1)
+        sub_df = sub_df[['text']]
         sub_df['label'] = label
+        sub_df['text'].replace('', np.nan, inplace=True)
+        sub_df.dropna(inplace=True)
 
         train_df, test_df = train_test_split(sub_df[['label', 'text']], test_size=test_pct)
         train_dfs.append(train_df)
         test_dfs.append(test_df)
 
-    train_df = pd.concat(train_dfs, ignore_index=True)
-    test_df = pd.concat(test_dfs, ignore_index=True)
+    train_df = shuffle(pd.concat(train_dfs, ignore_index=True))
+    test_df = shuffle(pd.concat(test_dfs, ignore_index=True))
 
     train_df.to_csv(os.path.join(output_dir, 'train.csv'), sep=sep, index=False)
     test_df.to_csv(os.path.join(output_dir, 'test.csv'), sep=sep, index=False)
