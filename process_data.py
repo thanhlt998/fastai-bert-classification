8d9a72af183d0606b6b87f9bf7ed666572441c15
thanhlt998 <thanhlt998@gmail.com>
2020-03-28 16:11:33 +0700

    First commit

---
+++ process_data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+import string
+
+
+def list2text(texts):
+    texts = list(texts)
+    res = []
+    for text in texts:
+        text = text.strip()
+        if not text: continue
+        if text[-1] in string.punctuation:
+            res.append(text)
+        else:
+            res.append(f'{text}.')
+    return ' '.join(res)
+
+
+def process(dir_name, output_dir, cols: list, test_pct=0.2, sep='\t'):
+    train_dfs, test_dfs = [], []
+    for fn in os.listdir(dir_name):
+        df = pd.read_csv(os.path.join(dir_name, fn), sep=sep)
+        label = fn.split('.')[0]
+        sub_df = df[cols]
+        sub_df.fillna(value='', inplace=True)
+        sub_df['text'] = sub_df.apply(lambda x: list2text(x), axis=1)
+        sub_df['label'] = label
+
+        train_df, test_df = train_test_split(sub_df[['label', 'text']], test_size=test_pct)
+        train_dfs.append(train_df)
+        test_dfs.append(test_df)
+
+    train_df = pd.concat(train_dfs, ignore_index=True)
+    test_df = pd.concat(test_dfs, ignore_index=True)
+
+    train_df.to_csv(os.path.join(output_dir, 'train.csv'), sep=sep, index=False)
+    test_df.to_csv(os.path.join(output_dir, 'test.csv'), sep=sep, index=False)
+
+
+if __name__ == '__main__':
+    input_dir = 'medical_data/raw_data'
+    output_dir = 'medical_data/title'
+    cols = ['title']
+    process(dir_name=input_dir, output_dir=output_dir, cols=cols)
+
+
